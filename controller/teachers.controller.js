031544a9e8a8005d249b92fe2b7baeddc29367f2
Sahil <jagadasahil@gmail.com>
2023-12-17 19:01:27 +0530

    Added Login, Registration functionality and remainig save tables backend

--- controller/teachers.controller.js
+++ controller/teachers.controller.js
@@ -1,7 +1,8 @@
 // BookPublications Controller
 import GenericController from './Generic_controller.js';
-import BookPublications from '../model/teachers.model.js';
+import { ResearchPublications, BookPublications, FacultyConferencePublications, Grants, ConsultancyReport, PatentPublications, ConferenceSeminars, SSTP_FDP_Workshop } from '../model/teachers.model.js';
 
+// BookPublications Controller
 export class BookPublicationsController extends GenericController {
   constructor() {
     super(BookPublications);
@@ -10,8 +11,52 @@ export class BookPublicationsController extends GenericController {
   // Specific methods for BookPublications controller2
 }
 
-class ReasearchPublications extends GenericController{
+// ResearchPublications Controller
+export class ReasearchPublicationsController extends GenericController{
     constructor(){
-        super("1__research_publication")
+        super(ResearchPublications);
     }
 }
+
+// FacultyConferencePublications Controller
+export class FacultyConferencePublicationsController extends GenericController{
+  constructor(){
+      super(FacultyConferencePublications);
+  }
+}
+
+// Grants Controller
+export class GrantsController extends GenericController{
+  constructor(){
+      super(Grants);
+  }
+}
+
+// ConsultancyReport Controller
+export class ConsultancyReportController extends GenericController{
+  constructor(){
+      super(ConsultancyReport);
+  }
+}
+
+// PatentPublications Controller
+export class PatentPublicationsController extends GenericController{
+  constructor(){
+      super(PatentPublications);
+  }
+}
+
+// ConferenceSeminars Controller
+export class ConferenceSeminarsController extends GenericController{
+  constructor(){
+      super(ConferenceSeminars);
+  }
+}
+
+// SSTP_FDP_Workshop Controller
+export class SSTP_FDP_WorkshopController extends GenericController{
+  constructor(){
+      super(SSTP_FDP_Workshop);
+  }
+}
+
